33426360d7f291255cb377480d85bca43f6e0b1c
Camilo Montoya :) <camilo.montoya.contractor@macmillan.com>
2024-04-13 13:43:59 -0500

    chore: remove unnecessary platform specification in Docker build command

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ version=$2
 echo "Building $account/ubuntu-sqitch:$version image..."
 
 # build the Docker image
-docker buildx build --platform linux/amd64,linux/arm64 -t $account/ubuntu-sqitch:$version --push .;
+docker buildx build -t $account/ubuntu-sqitch:$version --push .;
 
 # push the image
 docker push $account/ubuntu-sqitch:$version;
