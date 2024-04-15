9e819062cb8ff327a10867cab6404de169235d4b
Camilo Montoya :) <camilo.montoya.contractor@macmillan.com>
2024-04-15 15:29:52 -0500

    Refactor Dockerfile to include replace-project.sh script and update MySQL deployment script

--- build.sh
+++ build.sh
@@ -8,3 +8,9 @@ docker buildx build -t $account/ubuntu-sqitch:$version --push .;
 
 # push the image
 docker push $account/ubuntu-sqitch:$version;
+
+# build the Docker image for amd64
+docker buildx build --platform linux/amd64 -t $account/ubuntu-sqitch:$version-amd64 --push .;
+
+# push the image for amd64
+docker push $account/ubuntu-sqitch:$version-amd64;
