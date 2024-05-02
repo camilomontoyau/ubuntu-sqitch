2a0eb5363205f265e9a91f2e4f352fb0336b8c1b
Yeison Mateo Quintero <yeison.quintero@himedsolutions.com>
2024-05-02 16:36:58 -0500

    Cambios para deploy y verify al tiempo

--- build.sh
+++ build.sh
@@ -4,13 +4,13 @@ version=$2
 echo "Building $account/ubuntu-sqitch:$version image..."
 
 # build the Docker image
-docker buildx build -t $account/ubuntu-sqitch:$version --push .;
+docker buildx build -t $account/ubuntu-sqitch-mateo:$version --push .;
 
 # push the image
-docker push $account/ubuntu-sqitch:$version;
+docker push $account/ubuntu-sqitch-mateo:$version;
 
 # build the Docker image for amd64
-docker buildx build --platform linux/amd64 -t $account/ubuntu-sqitch:$version-amd64 --push .;
+docker buildx build --platform linux/amd64 -t $account/ubuntu-sqitch-mateo:$version-amd64 --push .;
 
 # push the image for amd64
-docker push $account/ubuntu-sqitch:$version-amd64;
+docker push $account/ubuntu-sqitch-mateo:$version-amd64;
